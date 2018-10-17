f3f21f3f675097a620ab87a2145adb357ede93bd
Diego Bezerra <diego.defb@gmail.com>
2018-10-17 12:17:23 -0400

    P3 TMC/PGIA7336

--- P3/oven/oven_c/_main.h
+++
@@ -1,9 +0,0 @@
-/* --- Generated the 17/10/2018 at 9:6 --- */
-/* --- heptagon compiler, version 1.03.00 (compiled thu. may. 3 2:35:29 CET 2018) --- */
-/* --- Command line: /usr/local/bin/heptc -target c -target z3z -s oven oven.ept --- */
-
-#ifndef _MAIN_H
-#define _MAIN_H
-
-#include "oven.h"
-#endif // _MAIN_H
